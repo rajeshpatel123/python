b387cc57d5f7c0e1435bd323add1ceab6b58e438
rajeshpatel123 <rajesh.patel123@gmail.com>
2026-06-30 11:19:22 +0530

    test-changes3

--- test-connection.py
+++ test-connection.py
@@ -11,7 +11,7 @@ app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 print('connected to firebase')
-print('and welcom to firebase')
+print('and welcom to firebases')
 bank_ref = db.collection("BankAccount")
 docs = bank_ref.stream()
 print(docs)
